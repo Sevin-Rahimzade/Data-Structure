1e5f6d3a3e269fd0c72365f57c2664a859c43cb7
XSherlockX <shiriamir2004@gmail.com>
2023-11-15 19:39:45 +0330

    Edit Array

--- Array (P2)/array_structure.py
+++ Array (P2)/array_structure.py
@@ -5,3 +5,18 @@ class Array:
         for i in range(arraySize):
             self.array.append(None)
             self.size = arraySize
+
+    def Insert(self, object, index):
+        if index < self.size:
+            self.array[index] = object
+            return index
+        else:
+            return -1
+
+    def Delete(self, index):
+        if index < self.size:
+            obj = self.array[index]
+            self.array[index] = None
+            return obj
+        else:
+            return None
