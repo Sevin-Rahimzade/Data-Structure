ffa92a364659e795298d343472027a9ba847134c
Sevin <seviinrahiim@gmail.com>
2023-11-15 20:26:41 +0330

    add methods

--- Queue/circular_queue.py
+++ Queue/circular_queue.py
@@ -4,3 +4,15 @@ class CircularQueue:
         self.size = size
         self.queue = [None for i in range(size)] 
         self.front = self.rear = -1
+
+
+    def isempty(self):
+        if self.front == -1:
+            return True
+        
+    def isfull(self):
+        if ((self.front == 0 and self.rear == -1) or ((self.rear+1) % self.size == self.front )):
+            return True
+
+    def peekQueue(self):
+        return self.queue[self.front]
