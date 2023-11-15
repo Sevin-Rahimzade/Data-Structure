2b7cfc099e16d24e93acd9736b4c707a6a0f846f
XSherlockX <shiriamir2004@gmail.com>
2023-11-15 20:52:06 +0330

    Add methods for Queue

--- Queue/queue.py
+++ Queue/queue.py
@@ -27,3 +27,35 @@ class QueueReplica:
 
     def peek(self):
         return self.queue[0]
+    
+    
+
+    def is_empty(self):
+        if self.counter == -1:
+            return True
+        else:
+            return False
+
+
+    def is_full(self):
+        if self.counter == self.size:
+            return True
+        else:
+            return False
+
+    def reverse(self):
+        return self.queue[::-1]
+
+
+
+# test
+obj = QueueReplica(5)
+obj.enqueue(2)
+obj.enqueue(4)
+obj.enqueue(3)
+print(obj.queue)
+
+obj.dequeue()
+print(obj.queue)
+
+print(obj.peek())
