d5f9818e55398ec47accd55f1c0a337c99bc1b12
Sevin <149188434+Sevin-Rahimzade@users.noreply.github.com>
2023-12-13 00:38:00 +0330

    add classes

--- Linked List/linked_list.py
+++ Linked List/linked_list.py
@@ -1,1 +1,11 @@
-print("Amir")
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList(object):
+
+    def __init__(self):
+        self.head = None
+        self.count = 0
