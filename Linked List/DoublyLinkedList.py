519cd69e65d942b47cc8b5c625ad0279299350e3
Sevin <149188434+Sevin-Rahimzade@users.noreply.github.com>
2023-12-13 00:40:40 +0330

    Added classes

--- Linked List/DoublyLinkedList.py
+++ Linked List/DoublyLinkedList.py
@@ -1,1 +1,9 @@
-print("Amir")
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
