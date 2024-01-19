4b81e4622c2d101b78b1724c9895d2acadf2d527
Sevin <149188434+Sevin-Rahimzade@users.noreply.github.com>
2024-01-19 22:48:41 +0330

    fixed

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -3,18 +3,22 @@ class stack:
     stack = list()
 
     def __init__(self, size):
-        for i in range(size):
-            self.stack.append(None)
-            self.size = size
+        self.stack = [None] * size
+        self.size = size
 
     def push(self, item):
-        self.items.append(item)
+        if not self.is_full():
+            self.top += 1
+            self.stack[self.top] = item
+        else:
+            raise Exception("Stack is full")
 
     def pop(self):
         if not self.is_empty():
-            element = self.items[-1]
-            del self.items[-1]
-            return element
+            element = self.stack[self.top]
+            del self.stack[self.top]
+            self.top -= 1
+            return element 
         else:
             raise Exception("Stack is empty")
             
@@ -22,10 +26,10 @@ class stack:
         return self.stack[self.top]
 
     def is_full(self):
-        if self.top == self.size:
+        if self.top == self.size -1:
             return True
             
     def is_empty(self):
         if self.top == -1:
             return True
-            
+        
